c72569ceb675a822aa3158c1bdc2177c32cb0180
Ganesh K <ganesh3597@gmail.com>
2018-12-29 16:58:43 +0530

    Added visited field

--- acre/loops.py
+++ acre/loops.py
@@ -1,12 +1,25 @@
 import json
 import sys
 import re
+from pprint import pprint
 
 from acre.ast_to_json import item_generator, file_to_dict
 
 class Loops:
 	def __init__(self, j_ast):
 		self.j_ast = j_ast
+		
+		
+	def _annotate_dict(self, json_input, nodetype):
+		if isinstance(json_input, dict):
+			for k in list(json_input.keys()):
+				if k == '_nodetype' and json_input[k] == nodetype:
+					json_input['visited'] = False
+				else:
+					(self._annotate_dict(json_input[k], nodetype))
+		elif isinstance(json_input, list):
+			for item in json_input:
+				self._annotate_dict(item, nodetype)
 	
 	def _get_level(self, body, nodetype, level):
 		_check = item_generator(body, '_nodetype', nodetype)
@@ -26,6 +39,8 @@ class Loops:
 		
 	def get_nested_level(self, nodetype):
 		levels = []
+		self._annotate_dict(self.j_ast, nodetype)
+		# pprint(self.j_ast)
 		for i, val in enumerate(item_generator(self.j_ast, '_nodetype', nodetype)):
 			levels.append(self._get_level(val, nodetype, 0))
 			
