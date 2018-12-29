068c26c5be8997e1d6790c76caaf03c174051ceb
Ganesh K <ganesh3597@gmail.com>
2018-12-29 10:48:25 +0530

    Update setup

--- setup.py
+++ setup.py
@@ -4,29 +4,23 @@ with open("README.md", "r") as fh:
    long_description = fh.read()
 
 setuptools.setup(
-    name="titanium_rhythm",
-    version="1.0.0",
-    author="Ganesh Kathiresan",
-    author_email="ganesh3597@gmail.com",
-    description="Automatic id3 modifier .mp3 files",
+    name="Acre",
+    version="0.0.1",
+    author="Ganesh Kathiresan, Rahul R Bharadwaj",
+    author_email="ganesh3597@gmail.com, rahulbharadwaj033@gmail.com",
+    description="Another Compiler Related Expansion",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/DarkFate13/titanium-rhythm",
-    packages=['titanium_rhythm'],
-    package_dir = {'titanium_rhythm': 'titanium_rhythm/'},
-    package_data={'titanium_rhythm': ['info/*.*', 'song_info/*.xml']},
+    url="https://github.com/Rahul-RB/Acre",
+    packages=['acre'],
+    package_dir = {'acre': 'acre/'},
+#     package_data={'titanium_rhythm': ['info/*.*', 'song_info/*.xml']},
     classifiers=(
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Attribution-ShareAlike 4.0 International",
         "Operating System :: OS Independent",
     ),
     install_requires=[
-          'discogs_client == 2.2.1',
-          'eyeD3 == 0.8.7',
-          'pyacoustid == 1.1.5',
-          'pytest == 3.6.1',
-          'requests == 2.18.4',
-          'setuptools == 38.6.0',
-          'validators == 0.12.2',
+         
       ],
 )
