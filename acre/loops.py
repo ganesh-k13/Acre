70fc07a1aa345174b87b91d7a735c575c89edbf9
Ganesh K <ganesh3597@gmail.com>
2018-12-29 09:02:13 +0530

    Basic get_count

--- acre/loops.py
+++ acre/loops.py
@@ -7,7 +7,13 @@ from ast_to_json import item_generator, file_to_dict
 class Loops:
 	def __init__(self, j_ast):
 		self.j_ast = j_ast
-
+	
+	def get_nested_level(self, nodetype):
+		for i, val in enumerate(item_generator(self.j_ast, '_nodetype', nodetype)):
+			pass
+		
+		return i+1
+		
 	def detect_nested_while_hard(self, level):
 		for i, val in enumerate(item_generator(self.j_ast, '_nodetype', 'While')):
 			pass
@@ -27,5 +33,6 @@ class Loops:
 if __name__ == '__main__':
 	ast_dict = file_to_dict(sys.argv[1])
 	l = Loops(ast_dict)
+	print(l.get_nested_level('While'))
 	print(l.detect_nested_while_hard(2))
 	print(l.detect_nested_while_soft(1))
